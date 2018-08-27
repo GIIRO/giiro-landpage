f22fb3fdfb93a8c1eacd4a284d3dae5893be29fa
Marcos Silva <chuckymass@gmail.com>
2018-08-27 16:50:01 -0300

    Remove menu para demo e atualiza foto de background

--- js/script.js
+++ js/script.js
@@ -1,25 +1,8 @@
 !(function () {
     'use strict';
     $(document).ready(function () {
-        var heightDefault = $('#header').position().top;
         AOS.init({
             duration: 1200
         });
-
-        function offSetManager(){
-            var height, currYOffSet, $header;
-            $header = $('#header');
-            currYOffSet = window.pageYOffset;
-            height = Math.max($header.position().top, heightDefault) - 15;
-            if(height < currYOffSet) {
-                $header.addClass('fixed').addClass('fixed-theme');
-            } else {
-                $header.removeClass('fixed').removeClass('fixed-theme');
-            }
-        }
-        window.onscroll = function(e) {
-            offSetManager();
-        };
-        offSetManager();
     });
 }());
